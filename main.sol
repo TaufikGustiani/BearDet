5ef3845d0c28c216b49cd49300c8547269178061
TaufikGustiani <148521173+TaufikGustiani@users.noreply.github.com>
2026-02-26 03:20:53 +0700

    Update main.sol

--- main.sol
+++ main.sol
@@ -54,3 +54,59 @@ contract BearDet is ReentrancyGuard, Ownable {
     error BRD_ZeroAddress();
     error BRD_ZeroAmount();
     error BRD_Halted();
+    error BRD_NotGuardian();
+    error BRD_NotReporter();
+    error BRD_TransferFailed();
+    error BRD_DrawdownOutOfRange();
+    error BRD_IndicatorOutOfRange();
+    error BRD_SeverityOutOfRange();
+    error BRD_ThresholdInvalid();
+    error BRD_SnapshotNotFound();
+    error BRD_MaxSnapshotsReached();
+    error BRD_ArrayLengthMismatch();
+    error BRD_BatchTooLarge();
+    error BRD_WithdrawZero();
+    error BRD_MaxSignalsReached();
+    error BRD_InvalidIndicatorId();
+    error BRD_InvalidIndex();
+    error BRD_AdvisoryNotFound();
+    error BRD_SignalNotFound();
+
+    uint256 public constant BRD_BPS_DENOM = 10000;
+    uint256 public constant BRD_MAX_INDICATORS = 16;
+    uint256 public constant BRD_MAX_SEVERITY = 5;
+    uint256 public constant BRD_MAX_DRAWDOWN_BPS = 10000;
+    uint256 public constant BRD_MAX_SNAPSHOTS = 50000;
+    uint256 public constant BRD_MAX_SIGNALS = 2000;
+    uint256 public constant BRD_BATCH_SIZE = 100;
+    uint256 public constant BRD_DOMAIN_SALT = 0xd4f6a8c0e2a4b6c8d0e2f4a6b8c0d2e4f6a8b0c2d4e6f8a0b2c4d6e8f0a2b4c6d8e0;
+
+    address public immutable brdTreasury;
+    uint256 public immutable deployBlock;
+    bytes32 public immutable chainDomain;
+
+    address public brdGuardian;
+    address public brdReporter;
+    bool public brdHalted;
+    uint256 public drawdownThresholdBps;
+    uint256 public snapshotCounter;
+    uint256 public signalCounter;
+    uint256 public advisoryCounter;
+    uint256 public treasuryBalance;
+    uint256 public reporterFeeWei;
+    mapping(uint8 => uint256) public indicatorThreshold;
+
+    struct DrawdownSnapshot {
+        address reporter;
+        uint256 drawdownBps;
+        uint256 peakValue;
+        uint256 currentValue;
+        uint256 atBlock;
+    }
+
+    struct ExitSignal {
+        uint8 indicatorId;
+        uint256 value;
+        uint256 threshold;
+        bytes32 labelHash;
+        uint256 atBlock;
